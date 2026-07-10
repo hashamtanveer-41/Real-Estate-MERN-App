ea8eec5ef21082b2171fe89772ce0f43e4508a7f
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-07-11 04:04:37 +0500

    add routing and create basic page components for Home, About, Signin, Signout, and Profile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
+import Signin from "./pages/Signin.jsx";
+import About from "./pages/About.jsx";
+import Home from "./pages/Home.jsx";
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Signout from "./pages/Signout.jsx";
+import Profile from "./pages/Profile.jsx";
+
+
 function App() {
   return (
-    <h1 className="text-2xl text-red-600">
-      hi
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/signin" element={<Signin />}/>
+        <Route path="/signout" element={<Signout />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/profile" element={<Profile />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
