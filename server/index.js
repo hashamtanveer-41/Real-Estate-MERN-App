6f520fa6f6be02479bb6b97693116acc82ceacb5
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-07-11 17:38:14 +0500

    connect to MongoDB and set DNS result order to prioritize IPv4

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+import * as dns from "node:dns";
+dotenv.config()
+dns.setDefaultResultOrder('ipv4first');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-dotenv.config()
+mongoose.connect(process.env.MONGO_URL)
+    .then(()=>{
+        console.log("Connected to MongoDB successfully")
+    }).catch((err)=>{
+    console.log(err)
+})
+
 app.listen(PORT, ()=>{
     console.log(`Server started successfully on PORT ${PORT}`)
 });
