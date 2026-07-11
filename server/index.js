0a62a9bd33cfa4a5b8dc0ec1cb2ef1ccc4f38318
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-07-11 17:48:53 +0500

    add authentication routes and user controller setup

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import * as dns from "node:dns";
+import userRoutes from "./routes/userRoutes.js";
 dotenv.config()
 dns.setDefaultResultOrder('ipv4first');
 
@@ -13,7 +14,9 @@ mongoose.connect(process.env.MONGO_URL)
         console.log("Connected to MongoDB successfully")
     }).catch((err)=>{
     console.log(err)
-})
+});
+
+app.use("/api/user",userRoutes)
 
 app.listen(PORT, ()=>{
     console.log(`Server started successfully on PORT ${PORT}`)
