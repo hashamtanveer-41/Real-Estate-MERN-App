f6d352aaf25daca77eeeb3daae4e45594031def2
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-07-15 13:04:19 +0500

    update profile component to display user information and add form for editing details

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import {useSelector} from "react-redux";
 
 const Profile = () => {
+    const {currentUser} = useSelector(state=>state.user)
     return (
-        <div>Profile</div>
+        <div className="p-3 max-w-lg mx-auto">
+            <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+            <form className="flex flex-col">
+                <img src={currentUser.avatar} alt="profile" className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2 "/>
+                <input className="border p-3 rounded-lg " type="text" id="username" placeholder="usernmae"/>
+                <input className="border p-3 rounded-lg " type="text" id="email" placeholder="email"/>
+                <input className="border p-3 rounded-lg " type="text" id="password" placeholder="password"/>
+                <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:bg-slate-600 disabled:opacity-95"></button>
+            </form>
+            <div className="flex justify-between mt-3">
+                <span className="text-red-700 cursor-pointer">Delete Account</span>
+                <span className="text-red-700 cursor-pointer">Sign Out</span>
+            </div>
+        </div>
     )
 }
 export default Profile
