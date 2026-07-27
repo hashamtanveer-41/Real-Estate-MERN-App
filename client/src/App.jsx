f10ca1f793f7360e9b35322d48bb2eabb12b2315
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-07-27 16:38:56 +0500

    add listing page with dynamic routing and Swiper integration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Header from "./components/Header.jsx";
 import PrivateRoutes from "./components/PrivateRoutes.jsx";
 import CreateListing from "./pages/CreateListing.jsx";
 import UpdateListing from "./pages/UpdateListing.jsx";
+import Listing from "./pages/Listing.jsx";
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
         <Route path="/sign-in" element={<Signin />}/>
         <Route path="/sign-up" element={<SignUp />}/>
         <Route path="/about" element={<About />}/>
+        <Route path="/listing/:id" element={<Listing />}/>
         <Route element={<PrivateRoutes />}>
             <Route path="/profile" element={<Profile />}/>
             <Route path="/create-listing" element={<CreateListing />}/>
