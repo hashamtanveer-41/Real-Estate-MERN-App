7d84ec8a177a7a35b7b164a48d139360455d94f9
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-07-29 15:54:49 +0500

    add initial files and configuration for project setup

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div>About</div>
+        <div className="py-20 px-4 max-w-6xl mx-auto">
+            <h1 className="text-3xl font-bold mb-4 text-slate-800">About HashamEstate</h1>
+            <p className="mb-4 text-slate-800">
+                HashamEstate is a leading real estate platform dedicated to helping individuals find their dream homes with ease. Our mission is to provide a seamless and user-friendly experience for buyers, sellers, and renters alike. With a wide range of properties available, we cater to diverse budgets and lifestyles, ensuring that everyone can find the perfect place to call home.
+            </p>
+            <p className="mb-4 text-slate-800">
+                Our team of experienced professionals is committed to delivering exceptional service and support throughout the entire real estate journey. Whether you're looking to buy, sell, or rent, HashamEstate is your trusted partner in making informed decisions and achieving your real estate goals.
+            </p>
+            <p className="mb-4 text-slate-800">
+                Join us today and discover the difference that HashamEstate can make in your real estate experience. Your dream home is just a click away!
+            </p>
+        </div>
     )
 }
 export default About
